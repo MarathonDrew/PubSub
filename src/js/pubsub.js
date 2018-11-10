af28238e8a17b4ef11ae4ff62db1cb0d1b4d4f92
Drew Hinderhofer <drew.hinderhofer@gmail.com>
2018-11-09 21:57:38 -0800

    commit from branch

--- src/js/pubsub.js
+++ src/js/pubsub.js
@@ -1,101 +1,97 @@
 ﻿// modified from the url http://davidwalsh.name/pubsub-javascript
-// test two 
+// test two commit from branch
 
 if (!Array.prototype.forEach) {
+  Array.prototype.forEach = function(callback, thisArg) {
+    var T, k;
 
-	Array.prototype.forEach = function (callback, thisArg) {
+    if (this == null) {
+      throw new TypeError(" this is null or not defined");
+    }
 
-		var T, k;
+    // 1. Let O be the result of calling ToObject passing the |this| value as the argument.
+    var O = Object(this);
 
-		if (this == null) {
-			throw new TypeError(' this is null or not defined');
-		}
+    // 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
+    // 3. Let len be ToUint32(lenValue).
+    var len = O.length >>> 0;
 
-		// 1. Let O be the result of calling ToObject passing the |this| value as the argument.
-		var O = Object(this);
+    // 4. If IsCallable(callback) is false, throw a TypeError exception.
+    // See: http://es5.github.com/#x9.11
+    if (typeof callback !== "function") {
+      throw new TypeError(callback + " is not a function");
+    }
 
-		// 2. Let lenValue be the result of calling the Get internal method of O with the argument "length".
-		// 3. Let len be ToUint32(lenValue).
-		var len = O.length >>> 0;
+    // 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
+    if (arguments.length > 1) {
+      T = thisArg;
+    }
 
-		// 4. If IsCallable(callback) is false, throw a TypeError exception.
-		// See: http://es5.github.com/#x9.11
-		if (typeof callback !== "function") {
-			throw new TypeError(callback + ' is not a function');
-		}
+    // 6. Let k be 0
+    k = 0;
 
-		// 5. If thisArg was supplied, let T be thisArg; else let T be undefined.
-		if (arguments.length > 1) {
-			T = thisArg;
-		}
+    // 7. Repeat, while k < len
+    while (k < len) {
+      var kValue;
 
-		// 6. Let k be 0
-		k = 0;
+      // a. Let Pk be ToString(k).
+      //   This is implicit for LHS operands of the in operator
+      // b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
+      //   This step can be combined with c
+      // c. If kPresent is true, then
+      if (k in O) {
+        // i. Let kValue be the result of calling the Get internal method of O with argument Pk.
+        kValue = O[k];
 
-		// 7. Repeat, while k < len
-		while (k < len) {
-
-			var kValue;
-
-			// a. Let Pk be ToString(k).
-			//   This is implicit for LHS operands of the in operator
-			// b. Let kPresent be the result of calling the HasProperty internal method of O with argument Pk.
-			//   This step can be combined with c
-			// c. If kPresent is true, then
-			if (k in O) {
-
-				// i. Let kValue be the result of calling the Get internal method of O with argument Pk.
-				kValue = O[k];
-
-				// ii. Call the Call internal method of callback with T as the this value and
-				// argument list containing kValue, k, and O.
-				callback.call(T, kValue, k, O);
-			}
-			// d. Increase k by 1.
-			k++;
-		}
-		// 8. return undefined
-	};
+        // ii. Call the Call internal method of callback with T as the this value and
+        // argument list containing kValue, k, and O.
+        callback.call(T, kValue, k, O);
+      }
+      // d. Increase k by 1.
+      k++;
+    }
+    // 8. return undefined
+  };
 }
 
 var pubsub;
-(function (global) {
-	'use strict';
-	
-	var pubsubModule = function () {
-		var topics = {};
-		var hOP = topics.hasOwnProperty;
+(function(global) {
+  "use strict";
 
-		var subscribe = function (topic, listener) {
-			// Create the topic's object if not yet created
-			if (!hOP.call(topics, topic)) topics[topic] = [];
+  var pubsubModule = function() {
+    var topics = {};
+    var hOP = topics.hasOwnProperty;
 
-			// Add the listener to queue
-			var index = topics[topic].push(listener) - 1;
+    var subscribe = function(topic, listener) {
+      // Create the topic's object if not yet created
+      if (!hOP.call(topics, topic)) topics[topic] = [];
 
-			// Provide handle back for removal of topic
-			return {
-				remove: function () {
-					delete topics[topic][index];
-				}
-			};
-		};
+      // Add the listener to queue
+      var index = topics[topic].push(listener) - 1;
 
-		var publish = function (topic, data) {
-			// If the topic doesn't exist, or there's no listeners in queue, just leave
-			if (!hOP.call(topics, topic)) return;
+      // Provide handle back for removal of topic
+      return {
+        remove: function() {
+          delete topics[topic][index];
+        }
+      };
+    };
 
-			// Cycle through topics queue, fire!
-			topics[topic].forEach(function (item) {
-				item((data != undefined) ? data : {});
-			});
-		};
+    var publish = function(topic, data) {
+      // If the topic doesn't exist, or there's no listeners in queue, just leave
+      if (!hOP.call(topics, topic)) return;
 
-		return {
-			subscribe: subscribe,
-			publish: publish
-		};
-	};
+      // Cycle through topics queue, fire!
+      topics[topic].forEach(function(item) {
+        item(data != undefined ? data : {});
+      });
+    };
 
-	global.pubsub = pubsubModule();
+    return {
+      subscribe: subscribe,
+      publish: publish
+    };
+  };
+
+  global.pubsub = pubsubModule();
 })(this);
